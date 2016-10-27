6b0b6af6b4c3936498b91df59fd8991cdf7b9cf0
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-27 16:04:12 +0100

    jQuery completed with colour changing text

--- jasmine/application.js
+++ jasmine/application.js
@@ -7,14 +7,46 @@ jQuery(document).ready(function () {
 
   $('#increase_temp').on('click', function () {
     thermostat.increase();
+    $('#energy_usage').text(thermostat._energyUsage);
+    energyColor();
     $('#current_temp').text(thermostat.currTemp);
   });
 
   $('#decrease_temp').on('click', function () {
     thermostat.decrease();
+    $('#energy_usage').text(thermostat._energyUsage);
+    energyColor();
     $('#current_temp').text(thermostat.currTemp);
   });
 
+  $('#reset').on('click', function () {
+    thermostat.reset();
+    $('#energy_usage').text(thermostat._energyUsage);
+    energyColor();
+    $('#current_temp').text(thermostat.currTemp);
+  });
 
+  $('#power_save').on('click', function () {
+    if(thermostat.powerSave === true) {
+      thermostat.isPowerSave(false);
+      $('h5').text("OFF");
+    }
+    else {
+      thermostat.isPowerSave(true);
+      $('h5').text("ON");
+    }
+  });
+
+  function energyColor() {
+    if(thermostat._energyUsage === "high-usage") {
+      $('#energy_usage').css('color', 'red');
+    }
+    else if(thermostat._energyUsage === "medium-usage") {
+      $('#energy_usage').css('color', 'black');
+    }
+    else {
+      $('#energy_usage').css('color', 'green');
+    }
+  }
 
 });
