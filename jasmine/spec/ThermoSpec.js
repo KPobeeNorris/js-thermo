971439dc9294ba4c494ababa84568779e9236c66
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-27 11:05:20 +0100

    Thermostat business logic section completed

--- jasmine/spec/ThermoSpec.js
+++ jasmine/spec/ThermoSpec.js
@@ -24,6 +24,12 @@ beforeEach(function() {
     expect(thermostat.minTemp).toEqual(10);
   });
 
+  it('should not allow users to reduce the temperature below 10 degrees', function () {
+    thermostat.currTemp = 10;
+    expect(function() {thermostat.decrease();}
+  ).toThrow("Minimum temperature reached");
+  });
+
   it('should have a max temperature of 25 degrees if power saving is on', function(){
     thermostat.isPowerSave(true);
     thermostat.setPowerSave();
@@ -34,6 +40,39 @@ beforeEach(function() {
     thermostat.isPowerSave(false);
     thermostat.setPowerSave();
     expect(thermostat.maxTemp).toEqual(32);
+  });
+
+  it('should not allow users to increase the temperature above the maximum temperature', function () {
+    thermostat.currTemp = 25;
+    expect(function() {thermostat.increase();}
+  ).toThrow("Maximum temperature reached");
+  });
+
+  it('should not allow users to increase the temperature above the maximum temperature', function () {
+    thermostat.powerSave = false;
+    thermostat.currTemp = 32;
+    expect(function() {thermostat.increase();}
+  ).toThrow("Maximum temperature reached");
+  });
+
+  it('should reset the temperature to 20', function () {
+    thermostat.reset();
+    expect(thermostat.currTemp).toEqual(20);
+  });
+
+  it('should advise the user they have a energy low usage', function(){
+    for(var i = 1; i < 5; i++) {thermostat.decrease();}
+    expect(thermostat._energyUsage).toEqual("low-usage");
+  });
+
+  it('should advise the user they have a energy medium usage', function(){
+    expect(thermostat._energyUsage).toEqual("medium-usage");
+  });
+
+  it('should advise the user they have a energy high usage', function(){
+    thermostat.powerSave = false;
+    for(var i = 1; i < 7; i++) {thermostat.increase();}
+    expect(thermostat._energyUsage).toEqual("high-usage");
   });
 
 });
--- jasmine/src/Thermo.js
+++ jasmine/src/Thermo.js
@@ -1,22 +1,32 @@
-
-
 function Thermostat () {
   this.currTemp = 20;
   this.minTemp = 10;
   this.maxTemp = 25;
   this.powerSave = true;
+  this._energyUsage = "medium-usage";
 }
 
-Thermostat.prototype.currTemp = function () {
-  var currTemp = 20;
-};
-
 Thermostat.prototype.increase = function () {
+  if(this.powerSave === true && this.currTemp === 25) {
+    throw "Maximum temperature reached";
+  }
+  else if(this.powerSave === false && this.currTemp === 32) {
+    throw "Maximum temperature reached";
+  }
+  else {
   this.currTemp ++;
+}
+this.energyUsage();
 };
 
 Thermostat.prototype.decrease = function () {
+  if(this.currTemp <= 10) {
+    throw ("Minimum temperature reached");
+  }
+  else {
   this.currTemp --;
+}
+this.energyUsage();
 };
 
 Thermostat.prototype.setPowerSave = function() {
@@ -29,4 +39,21 @@ Thermostat.prototype.setPowerSave = function() {
 
 Thermostat.prototype.isPowerSave = function (state) {
   this.powerSave = state;
+};
+
+Thermostat.prototype.reset = function () {
+  this.currTemp = 20;
+  this.energyUsage();
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if(this.currTemp < 18) {
+    this._energyUsage = "low-usage";
+  }
+  else if(this.currTemp < 25) {
+    this._energyUsage = "medium-usage";
+  }
+  else {
+    this._energyUsage = "high-usage";
+  }
 };
